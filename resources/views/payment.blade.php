a1d867265d2e805e7489546d8177dd54a43aebdc
GeloParlade <30657437+GeloParlade@users.noreply.github.com>
2020-05-09 20:39:00 +1000

    Updates Booking and Vehicle Database

--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -15,18 +15,70 @@ ob_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Payment</title>
+    <script src="https://www.gstatic.com/firebasejs/7.14.0/firebase-app.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/7.14.0/firebase-firestore.js"></script>
+
+    <script>
+    var firebaseConfig = {
+        apiKey: "AIzaSyC2ZMCg8GIWJeW1Y5n3cjsQ4Wk1fDM4J-8",
+        authDomain: "car-for-all-273711.firebaseapp.com",
+        databaseURL: "https://car-for-all-273711.firebaseio.com",
+        projectId: "car-for-all-273711",
+        storageBucket: "car-for-all-273711.appspot.com",
+        messagingSenderId: "548693459929",
+        appId: "1:548693459929:web:3b914dd957d24cf9358fc8"
+    };
+
+    firebase.initializeApp(firebaseConfig);
+    var db = firebase.firestore();
+
+    function book() {
+
+        db.collection("Bookings").add({
+            carID: "<?php echo $carID ?>",
+            datefrom: "<?php echo $datefrom ?>",
+            dateto: "<?php echo $dateto ?>",
+            name: "<?php echo $name ?>",
+            email: "<?php echo $email ?>",
+            phone: "<?php echo $phone ?>"
+        })
+        .then(function(docRef) {
+            console.log("Document written with ID: ", docRef.id);
+        })
+        .catch(function(error) {
+            console.error("Error adding document: ", error);
+        });
+
+        db.collection("Vehicles").doc("<?php echo $carID ?>").update({
+            available: false
+        })
+        .then(function() {
+            console.log("Document successfully updated!");
+        })
+        .catch(function(error) {
+            // The document probably doesn't exist.
+            console.error("Error updating document: ", error);
+        });
+
+    }
+
+    
+
+    </script>
+
 </head>
 
 <body>
 
     <div class="center">
-        <h1>Payment</h1>
+        <h1>Payment<?php echo $carID ?></h1>
     </div>
     <div class="center">
         <div class="texty">
             <p>We currently Only accept Paypal as a payment method, as it is very secure</p>
             <p>If you do not have an paypal account, you can pay as a guest</p>
             <p>and we well store your credit card details!</p>
+            <button onClick="book()"><span style="color: #000000;"> PAY</span> </button>
 
         </div>
     </div>
