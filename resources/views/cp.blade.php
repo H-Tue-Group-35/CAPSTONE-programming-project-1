27517208559aedc54a8cfb9acf4396a131dfef6a
Ryan Babij <rbabij@gmail.com>
2020-05-09 20:14:32 +1000

    CSS try 8

--- resources/views/cp.blade.php
+++ resources/views/cp.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	{{ HTML::style('css/style.css') }}
+	<link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}" >
 	
     <title>Admin Control Panel</title>
 	
