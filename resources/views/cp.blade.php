347e03fa116cabfeeb0e85281f24dba1e78a82f1
Ryan Babij <rbabij@gmail.com>
2020-05-10 21:25:55 +1000

    Update cp.blade.php

--- resources/views/cp.blade.php
+++ resources/views/cp.blade.php
@@ -160,14 +160,19 @@
 		// const firebaseAdmin = require('firebase-admin');
 
 		// new GeoPoint ( latitude :  number ,  longitude :  number ) : GeoPoint
+		
+		var vModel = document.getElementById('model').value;
 
 		db.collection("Vehicles").add({
 		available: "true",
 		image: "https://firebasestorage.googleapis.com/v0/b/car-for-all-273711.appspot.com/o/Car%20Pictures%2Fcorolla.png?alt=media&token=84eb8d77-91a4-469a-b502-78fdac83ae6a",
 		/* myLocation: new firebaseAdmin.firestore.GeoPoint(0,0), */
-		model: 'test',
+		model: vModel,
 		seats: 5
 		});
+		
+		
+		
 	}
 	
     </script>
@@ -185,7 +190,11 @@
 	
 	<h2>Make new Vehicle (also random option)</h2>
 	
+		Model: <input type="text" name="model" id="model" maxlength="12" required>
+		<br/>
+	
 	<button onclick="makeVehicle()">Make vehicle</button>
+
 	
 	<h2>Emergency map</h2>
 	
