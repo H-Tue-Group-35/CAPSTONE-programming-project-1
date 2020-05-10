762369b775c4114267b973cc7aefb58fe96dd145
Ryan Babij <rbabij@gmail.com>
2020-05-10 21:12:11 +1000

    Update cp.blade.php

--- resources/views/cp.blade.php
+++ resources/views/cp.blade.php
@@ -156,18 +156,18 @@
 	function makeVehicle()
 	{
 		alert("test");
-		    var db = firebase.firestore();
-		const firebaseAdmin = require('firebase-admin');
+		    // var db = firebase.firestore();
+		// const firebaseAdmin = require('firebase-admin');
 		
-		new GeoPoint ( latitude :  number ,  longitude :  number ) : GeoPoint
+		// new GeoPoint ( latitude :  number ,  longitude :  number ) : GeoPoint
 		
-		db.collection("Vehicles").add({
-			available: "true",
-			image: "https://firebasestorage.googleapis.com/v0/b/car-for-all-273711.appspot.com/o/Car%20Pictures%2Fcorolla.png?alt=media&token=84eb8d77-91a4-469a-b502-78fdac83ae6a",
-			myLocation: new firebaseAdmin.firestore.GeoPoint(0,0),
-			model: 'test',
-			seats: 5
-		});
+		// db.collection("Vehicles").add({
+			// available: "true",
+			// image: "https://firebasestorage.googleapis.com/v0/b/car-for-all-273711.appspot.com/o/Car%20Pictures%2Fcorolla.png?alt=media&token=84eb8d77-91a4-469a-b502-78fdac83ae6a",
+			// myLocation: new firebaseAdmin.firestore.GeoPoint(0,0),
+			// model: 'test',
+			// seats: 5
+		// });
 	}
 	
     </script>
