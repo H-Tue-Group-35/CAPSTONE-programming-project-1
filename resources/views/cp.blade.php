b52634cc9b36ae96c340e34d2d211553335da748
Ryan Babij <rbabij@gmail.com>
2020-05-12 00:46:50 +1000

    Fix admin map formatting

--- resources/views/cp.blade.php
+++ resources/views/cp.blade.php
@@ -32,22 +32,28 @@ ob_start();
     var db = firebase.firestore();
     var map, infoWindow;
 
-    function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
+    function initMap()
+	{
+        map = new google.maps.Map(document.getElementById('map'),
+		{
             center: {
                 lat: -37.806,
                 lng: 144.954
             },
             zoom: 12
-        });
+        }
+		);
         infoWindow = new google.maps.InfoWindow;
 
         // Try HTML5 geolocation.
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function(position) {
+        if (navigator.geolocation) 
+		{
+            navigator.geolocation.getCurrentPosition(function(position)
+			{
                 console.log(position.coords.latitude);
 
-                var pos = {
+                var pos =
+				{
                     lat: position.coords.latitude,
                     lng: position.coords.longitude
                 };
@@ -58,114 +64,121 @@ ob_start();
                     map: map
                 });
                 map.setCenter(pos);
+			},
+			function()
+			{
+				handleLocationError(true, infoWindow, map.getCenter());
+			}
+			);
+		}
+		else
+		{
+			// Browser doesn't support Geolocation
+			handleLocationError(false, infoWindow, map.getCenter());
+		}
 
 
-                var markers = []
-                console.log(typeof(markers));
+
+		var markers = []
+		console.log(typeof(markers));
 
 
-                db.collection("Vehicles")
-                    .get()
-                    .then(function(querySnapshot) {
-                        querySnapshot.forEach(function(doc) {
-                            if (doc.data().available) {
-                                var coordinates = {
-                                    lat: doc.data().location.latitude,
-                                    lng: doc.data().location.longitude
-                                };
-								
-                                var contentString =
-                                    '<p><span style="color: #000000;"><img style="display: block; margin-left: auto; margin-right: auto;" src="' +
-                                    doc.data().image +
-                                    '" alt="" width="246" height="138" /></span></p>' +
-                                    '<p style="text-align: center;"><span style="color: #000000;">Brand: ' +
-                                    doc.data().brand + '</span></p>' +
-                                    '<p style="text-align: center;"><span style="color: #000000;">Model: ' +
-                                    doc.data().model + '</span></p>' +
-                                    '<p style="text-align: center;"><span style="color: #000000;">Seats: ' +
-                                    doc.data().seats + '</span></p>' +
-                                    '<p style="text-align: center;"><span style="color: #000000;">Available!</span></p>' +
-                                    '<p style="text-align: center;"><span style="color: #000000;">&nbsp;</span></p>' +
-                                    '<form onsubmit="test()">' +
-                                    '<p style="text-align: center;"><input type="hidden" name="carID" value="' +
-                                    doc.id +
-                                    '" ><button onclick="vehicleEmergency()">Summon emergency services</button>' +
-									'<button onclick="vehicleDeactivate()">Deactivate</button>' +
-									'<button onclick="vehicleDelete()">Delete</button></p></form>';
+		db.collection("Vehicles").get().then(function(querySnapshot)
+		{
+			querySnapshot.forEach(function(doc)
+			{
+				if (doc.data().available)
+				{
+					var coordinates =
+					{
+						lat: doc.data().location.latitude,
+						lng: doc.data().location.longitude
+					};
 
-                                var carInfo = new google.maps.InfoWindow({
-                                    content: contentString
-                                });
+					var contentString =
+					'<p><span style="color: #000000;"><img style="display: block; margin-left: auto; margin-right: auto;" src="' +
+					doc.data().image +
+					'" alt="" width="246" height="138" /></span></p>' +
+					'<p style="text-align: center;"><span style="color: #000000;">Brand: ' +
+					doc.data().brand + '</span></p>' +
+					'<p style="text-align: center;"><span style="color: #000000;">Model: ' +
+					doc.data().model + '</span></p>' +
+					'<p style="text-align: center;"><span style="color: #000000;">Seats: ' +
+					doc.data().seats + '</span></p>' +
+					'<p style="text-align: center;"><span style="color: #000000;">Available!</span></p>' +
+					'<p style="text-align: center;"><span style="color: #000000;">&nbsp;</span></p>' +
+					'<form onsubmit="test()">' +
+					'<p style="text-align: center;"><input type="hidden" name="carID" value="' +
+					doc.id +
+					'" ><button onclick="vehicleEmergency()">Summon emergency services</button>' +
+					'<button onclick="vehicleDeactivate()">Deactivate</button>' +
+					'<button onclick="vehicleDelete()">Delete</button></p></form>';
 
-                                var carMarker = new google.maps.Marker({
-                                    position: coordinates,
-                                    map: map,
-                                    icon: {
-                                        url: "http://maps.google.com/mapfiles/kml/pal4/icon15.png"
-                                    }
-                                });
-                                carMarker.addListener('click', function() {
-                                    carInfo.open(map, carMarker);
-                                });
+					var carInfo = new google.maps.InfoWindow({
+					content: contentString
+					});
 
-                                markers.push(carMarker);
-                            } else // if car is not available, show debug marker for now
-                            {
+					var carMarker = new google.maps.Marker({
+					position: coordinates,
+					map: map,
+					icon: {
+					url: "http://maps.google.com/mapfiles/kml/pal4/icon15.png"
+					}
+					});
+					carMarker.addListener('click', function() {
+					carInfo.open(map, carMarker);
+					});
 
-                                var coordinates = {
-                                    lat: doc.data().location.latitude,
-                                    lng: doc.data().location.longitude
-                                };
+					markers.push(carMarker);
+				}
+				else // if car is not available, show debug marker for now
+				{
 
-                                var contentString =
-                                    '<p><span style="color: #000000;"><img style="display: block; margin-left: auto; margin-right: auto;" src="' +
-                                    doc.data().image +
-                                    '" alt="" width="246" height="138" /></span></p>' +
-                                    '<p style="text-align: center;"><span style="color: #000000;">Brand: ' +
-                                    doc.data().brand + '</span></p>' +
-                                    '<p style="text-align: center;"><span style="color: #000000;">Model: ' +
-                                    doc.data().model + '</span></p>' +
-                                    '<p style="text-align: center;"><span style="color: #000000;">Seats: ' +
-                                    doc.data().seats + '</span></p>' +	
-                                    '<p style="text-align: center;"><span style="color: #000000;">Available!</span></p>' +
-                                    '<p style="text-align: center;"><span style="color: #000000;">&nbsp;</span></p>' +
-                                    '<form onsubmit="test()">' +
-                                    '<p style="text-align: center;"><input type="hidden" name="carID" value="' +
-                                    doc.id +
-                                    '" ><button onclick="vehicleEmergency()">Summon emergency services</button>' +
-									'<button onclick="vehicleActivate()">Activate</button>' +
-									'<button onclick="vehicleDelete()">Delete</button></p></form>';
+					var coordinates = 
+					{
+						lat: doc.data().location.latitude,
+						lng: doc.data().location.longitude
+					};
 
-                                var carInfo = new google.maps.InfoWindow({
-                                    content: contentString
-                                });
+					var contentString =
+					'<p><span style="color: #000000;"><img style="display: block; margin-left: auto; margin-right: auto;" src="' +
+					doc.data().image +
+					'" alt="" width="246" height="138" /></span></p>' +
+					'<p style="text-align: center;"><span style="color: #000000;">Brand: ' +
+					doc.data().brand + '</span></p>' +
+					'<p style="text-align: center;"><span style="color: #000000;">Model: ' +
+					doc.data().model + '</span></p>' +
+					'<p style="text-align: center;"><span style="color: #000000;">Seats: ' +
+					doc.data().seats + '</span></p>' +	
+					'<p style="text-align: center;"><span style="color: #000000;">Available!</span></p>' +
+					'<p style="text-align: center;"><span style="color: #000000;">&nbsp;</span></p>' +
+					'<form onsubmit="test()">' +
+					'<p style="text-align: center;"><input type="hidden" name="carID" value="' +
+					doc.id +
+					'" ><button onclick="vehicleEmergency()">Summon emergency services</button>' +
+					'<button onclick="vehicleActivate()">Activate</button>' +
+					'<button onclick="vehicleDelete()">Delete</button></p></form>';
 
-                                var carMarker = new google.maps.Marker({
-                                    position: coordinates,
-                                    map: map,
-                                    icon: {
-                                        url: "http://maps.google.com/mapfiles/kml/pal3/icon45.png"
-                                    }
-                                });
-                                carMarker.addListener('click', function() {
-                                    carInfo.open(map, carMarker);
-                                });
+					var carInfo = new google.maps.InfoWindo
+					({ content: contentString });
 
-                                markers.push(carMarker);
-                            }
-                        });
-                    })
-                    .catch(function(error) {
-                        console.log("Error getting documents: ", error);
-                    });
+					var carMarker = new google.maps.Marke
+					({
+						position: coordinates,
+						map: map,
+						icon: { url: "http://maps.google.com/mapfiles/kml/pal3/icon45.png" }
+					});
+					
+					carMarker.addListener('click', function()
+					{ carInfo.open(map, carMarker); });
 
-            }, function() {
-                handleLocationError(true, infoWindow, map.getCenter());
-            });
-        } else {
-            // Browser doesn't support Geolocation
-            handleLocationError(false, infoWindow, map.getCenter());
-        }
+					markers.push(carMarker);
+				}
+			});
+		}).catch(function(error)
+		{
+			console.log("Error getting documents: ", error);
+		});
     }
 
     function handleLocationError(browserHasGeolocation, infoWindow, pos) {
