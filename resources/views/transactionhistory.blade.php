153eda1489727ffb143d4c2dc28b81c0cd491374
mehyarbelal1@hotmail.com <mehyarbelal1@hotmail.com>
2020-05-14 03:55:46 +1000

    Update transactionhistory.blade.php

--- resources/views/transactionhistory.blade.php
+++ resources/views/transactionhistory.blade.php
@@ -1,3 +1,9 @@
+<?php
+
+
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,17 +11,85 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Transaction history</title>
+
+    <script src="https://www.gstatic.com/firebasejs/7.14.0/firebase-app.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/7.14.0/firebase-firestore.js"></script>
+
+
+
+
+
 </head>
+
+
+<style>
+.cener {
+    text-align: center;
+}
+</style>
 
 <body>
 
     <h1> This is the transaction History page</h1>
+
+
+    <div class="center">
+
+        <ul class="bookings"></ul>
+
+
+    </div>
     <script>
     // db.collection("user").get().then((snapshot) => {
     // snapshot.docs.forEach(doc => {
     // console.log(doc.data())
     // })
     // })
+    </script>
+
+    <script>
+    var firebaseConfig = {
+        apiKey: "AIzaSyC2ZMCg8GIWJeW1Y5n3cjsQ4Wk1fDM4J-8",
+        authDomain: "car-for-all-273711.firebaseapp.com",
+        databaseURL: "https://car-for-all-273711.firebaseio.com",
+        projectId: "car-for-all-273711",
+        storageBucket: "car-for-all-273711.appspot.com",
+        messagingSenderId: "548693459929",
+        appId: "1:548693459929:web:3b914dd957d24cf9358fc8"
+    };
+
+    firebase.initializeApp(firebaseConfig);
+    var db = firebase.firestore();
+
+
+
+    const users = document.getElementById("users");
+
+    function renderuser(doc) {
+
+        let li = document.createElement("li");
+        let username = document.createElement("span");
+        let password = document.createElement("span");
+
+        li.setAttribute("data-id", doc.id);
+
+        username.textContent = doc.data().username;
+        password.textContent = doc.data().password;
+
+        li.appendChild(username)
+        li.appendChild(password)
+
+        users.appendChild(li)
+
+    }
+
+
+    db.collection("user").get().then((snapshot) => {
+        snapshot.docs.forEach(doc => {
+            console.log(doc.data())
+            renderuser(doc);
+        })
+    })
     </script>
 
 </body>
