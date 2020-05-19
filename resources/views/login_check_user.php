316a43b9e7df8f2428bbd18dddc74fea349d30a0
Ryan Babij <rbabij@gmail.com>
2020-05-19 23:42:16 +1000

    Update login_check_user.php

--- resources/views/login_check_user.php
+++ resources/views/login_check_user.php
@@ -7,16 +7,5 @@ ob_start();
 sessionStorage.loginid = "<?php echo $username ?>";
 
 console.log("SESSION: "+sessionStorage.loginid);
+window.location.replace("https://car-for-all-273711.appspot.com/");
 </script>
-
-<?php
-	if (strcmp($username,"admin") === 0 && strcmp($password,"admin") === 0)
-	{
-		// password is correct, redirect to cp
-		//header('Location: https://car-for-all-273711.appspot.com/');
-		exit();
-	}
-
-//header('Location: https://car-for-all-273711.appspot.com/');
-exit();
-?>
