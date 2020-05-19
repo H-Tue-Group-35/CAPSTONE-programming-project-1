2c7dc9a360473b82e7c4483d527676e95d12a030
Ryan Babij <rbabij@gmail.com>
2020-05-19 23:31:52 +1000

    Revert "Update login_check_user.php"
    This reverts commit 59efd2e38f75ba50911c48d78e01fffa79aaecbf.

--- resources/views/login_check_user.php
+++ resources/views/login_check_user.php
@@ -6,7 +6,6 @@ ob_start();
 <script>
 sessionStorage.loginid = "<?php echo $username ?>";
 
-//console.log("uname: "+sessionStorage.loginid);
 console.log("SESSION: "+sessionStorage.loginid);
 </script>
 
@@ -14,10 +13,10 @@ console.log("SESSION: "+sessionStorage.loginid);
 	if (strcmp($username,"admin") === 0 && strcmp($password,"admin") === 0)
 	{
 		// password is correct, redirect to cp
-		//header('Location: https://car-for-all-273711.appspot.com/');
+		header('Location: https://car-for-all-273711.appspot.com/');
 		exit();
 	}
 
-//header('Location: https://car-for-all-273711.appspot.com/');
+header('Location: https://car-for-all-273711.appspot.com/');
 exit();
 ?>
