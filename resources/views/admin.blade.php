991f491069c8b98faf778525c50e284562812545
Ryan Babij <rbabij@gmail.com>
2020-05-19 15:21:45 +1000

    test cci

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -8,7 +8,7 @@ ob_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin</title>
+    <title>Admin t</title>
 </head>
 
 <body>
