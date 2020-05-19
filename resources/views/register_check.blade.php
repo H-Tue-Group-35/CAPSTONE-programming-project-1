95a5fa3c0f1bc661a38ce1042ab8960f9a8b58fd
Ryan Babij <rbabij@gmail.com>
2020-05-19 19:51:19 +1000

    Update register_check.blade.php

--- resources/views/register_check.blade.php
+++ resources/views/register_check.blade.php
@@ -5,4 +5,37 @@ ob_start();
 exit();
 ?>
 
-Check registration here.
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+    <title>Registering</title>
+	
+
+</head>
+
+
+    <script>
+    var firebaseConfig = {
+        apiKey: "AIzaSyC2ZMCg8GIWJeW1Y5n3cjsQ4Wk1fDM4J-8",
+        authDomain: "car-for-all-273711.firebaseapp.com",
+        databaseURL: "https://car-for-all-273711.firebaseio.com",
+        projectId: "car-for-all-273711",
+        storageBucket: "car-for-all-273711.appspot.com",
+        messagingSenderId: "548693459929",
+        appId: "1:548693459929:web:3b914dd957d24cf9358fc8"
+    };
+
+    firebase.initializeApp(firebaseConfig);
+    var db = firebase.firestore();
+    var map, infoWindow;
+	
+	
+	</script>
+
+<body>
+Registering...
+</body>
