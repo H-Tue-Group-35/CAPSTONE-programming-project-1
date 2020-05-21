6021e739f40321018cd532d30eb965c5d591bbe7
mehyarbelal1@hotmail.com <mehyarbelal1@hotmail.com>
2020-05-22 09:56:57 +1000

    materizle framework added

--- resources/views/booking.blade.php
+++ resources/views/booking.blade.php
@@ -12,7 +12,8 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link href="/style.css" rel="stylesheet" type="text/css" >
+    <link href="/style.css" rel="stylesheet" type="text/css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     <title>Book now!</title>
 
 
@@ -155,7 +156,7 @@ session_start();
         color: red;
     }
     </style>
-	
+
 </body>
 
 </html>
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,67 +1,70 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link href="/style.css" rel="stylesheet" type="text/css" >
+    <link href="/style.css" rel="stylesheet" type="text/css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     <title>Contact Us</title>
+
+
 </head>
+
 <body>
-    
+
     <style>
-    
-h1{
-    font-size: 3.5em;
-    text-shadow: 2px 2px black;
-    text-align: center;
-    text-decoration: underline;
-}    
+    h1 {
+        font-size: 3.5em;
+        text-shadow: 2px 2px black;
+        text-align: center;
+        text-decoration: underline;
+    }
 
-.center{
-    text-align: center;
-    border: 2px solid black;
-}
-
-
+    .center {
+        text-align: center;
+        border: 2px solid black;
+    }
     </style>
 
 
     <h1>Contact Us</h1>
 
-<div class="center">
+    <div class="center">
 
-<section>
-                        <div class="contact-method">
-                            
-                            <h3>Email</h3>
-                            <a href="#">Car4all@gmail.com</a>
-                        </div>
-                    </section>
-                    <section>
-                        <div class="contact-method">
-                         
-                            <h3>Phone</h3>
-                            <span>(03) 100-5555 12387</span>
-                            <br>
-                            <span>(04) 450-0400 12387</span>
-                        </div>
-                    </section>
-                    <section>
-                        <div class="contact-method">
-                           
-                            <h3>Address</h3>
-                            <span>1234 Collins Street #5432<br />
-                                Melbourne, CBD 3051<br />
-                                Australia</span>
-                        </div>
-                    </section>
+        <section>
+            <div class="contact-method">
+
+                <h3>Email</h3>
+                <a href="#">Car4all@gmail.com</a>
+            </div>
+        </section>
+        <section>
+            <div class="contact-method">
+
+                <h3>Phone</h3>
+                <span>(03) 100-5555 12387</span>
+                <br>
+                <span>(04) 450-0400 12387</span>
+            </div>
+        </section>
+        <section>
+            <div class="contact-method">
+
+                <h3>Address</h3>
+                <span>1234 Collins Street #5432<br />
+                    Melbourne, CBD 3051<br />
+                    Australia</span>
+            </div>
+        </section>
 
 
-</div>
+    </div>
 
 
 
 
 
 </body>
+
 </html>
