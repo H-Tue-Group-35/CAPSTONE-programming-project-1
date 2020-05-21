8821ad6d6b0716cccd4221a6805a39b7df9b7764
mehyarbelal1@hotmail.com <mehyarbelal1@hotmail.com>
2020-05-22 08:16:41 +1000

    fonts added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,6 +25,7 @@ ob_start();
     <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet">
     <script>
     //sessionStorage.loginid = "none";
 
