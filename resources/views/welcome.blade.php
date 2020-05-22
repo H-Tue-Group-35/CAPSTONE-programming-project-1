9815e914d7076553e7b256d4b738044c5df9e1fb
mehyarbelal1@hotmail.com <mehyarbelal1@hotmail.com>
2020-05-22 10:29:08 +1000

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -341,7 +341,7 @@ $_SESSION["login_id"] = "TEST";
         <div class="row">
             <div class="col l6 s12">
                 <h5 class="white-text">Footer Content</h5>
-                <p class="grey-text text-lighten-4">Car4all.
+                <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.
                 </p>
             </div>
             <div class="col l4 offset-l2 s12">
