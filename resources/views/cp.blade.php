251be1d4d4e84452c8630b73bdddf21d903f380e
Ryan Babij <rbabij@gmail.com>
2020-05-26 12:11:53 +1000

    Update admin style

--- resources/views/cp.blade.php
+++ resources/views/cp.blade.php
@@ -303,91 +303,24 @@ ob_start();
 		Coordinates: <input type="text" name="fLat" id="fLat" maxlength="12">
 					 <input type="text" name="fLong" id="fLong" maxlength="12">
 		</div>
+		<div class="field half">
 		Status:
 		<select id="fStatus" name="fStatus">
 		  <option value="fActive">Active</option>
 		  <option value="fInactive">Inactive</option>
 		</select>
+		</div>
 
 		<br/>
 		<input type="submit">
 		</form>
 		</div>
-
-        <form method="post" action="payment" name="form" id="form" onsubmit="return validateForm()" autocomplete="on">
-            <div class="fields">
-
-
-                <div class="field half">
-                    <label for="date-from">Date from</label>
-                    <input type="date" name="datefrom" id="date-from" onchange="changeMinDate(this.value)" required/>
-                </div>
-
-                <div class="field half">
-                    <label for="date-to">Date to</label>
-                    <input type="date" name="dateto" id="date-to" onchange="changeMaxDate(this.value)" required/>
-                </div>
-
-                <div class="field">
-                    <label for="name">Name</label>
-                    <input type="text" name="name" id="name" placeholder="Name" required />
-                </div>
-
-                <div class="field half">
-                    <label for="email">Email</label>
-                    <input type="email" name="email" id="email" placeholder="Email" required/>
-                </div>
-
-                <div class="field half">
-                    <label for="phone">Phone</label>
-                    <input type="tel" name="phone" id="phone" placeholder="10 digit mobile number" pattern="[0-9]{10}" required/>
-                </div>
-
-
-
-
-
-
-            </div>
-
-            <div class="center">
-
-                <div class="field half text-right">
-                    <label>&nbsp;</label>
-
-
-                    <input type="hidden" name="carID" value="<?php echo  $carid; ?>">
-                    <input type="submit" value="Proceed to Checkout" class="primary" name="submit" />
-
-
-
-                </div>
-
-            </div>
-        </form>
-
-
-
-
+		
+		<h2>Vehicle map</h2>
+		
+		<div id="map" style="height:800px;"></div>
 
     </div>
-
-
-	<h1>Admin Control Panel</h1>
-
-	<h2>Fleet Status</h2>
-	
-	<p>List all vehicles here for easy management. Add ability to make active/inactive or delete.</p>
-
-	<h2>Make new Vehicle (empty coordinates will give random position in city)</h2>
-
-
-	<br/>
-
-	<h2>Vehicle map</h2>
-	<p>Delete vehicles or summon emergency services.</p>
-
-	<div id="map" style="height:800px;"></div>
 
 </body>
 </html>
