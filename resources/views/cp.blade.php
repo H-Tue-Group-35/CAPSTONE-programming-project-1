ac73fec827729edb6f70680c7904666f72af7be6
Ryan Babij <rbabij@gmail.com>
2020-05-31 21:39:53 +1000

    Update cp.blade.php

--- resources/views/cp.blade.php
+++ resources/views/cp.blade.php
@@ -21,6 +21,8 @@ ob_start();
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+	<!-- Jquery -->
+	<script type="text/javascript" src="https://code.jquery.com/jquery-1.7.1.min.js"></script>
 
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet">
 	
