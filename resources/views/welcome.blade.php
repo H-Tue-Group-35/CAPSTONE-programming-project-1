327bdfc581feb720f0912f94e19e8e7b199cfa16
mehyarbelal1@hotmail.com <mehyarbelal1@hotmail.com>
2020-06-01 18:09:15 +1000

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,8 +11,9 @@ ob_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="/style.css" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css" />
+
+    <link href="/style.css" rel="stylesheet" type="text/css">
     <title>Car4All</title>
 
 
