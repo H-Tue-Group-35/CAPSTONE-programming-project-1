a9fd1c51659845d4c541625b63485c6871658cb3
Ryan Babij <rbabij@gmail.com>
2020-06-13 19:08:51 +1000

    Style cleanup
    * Add style to Register

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,95 +6,93 @@ ob_start();
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link href="/style.css" rel="stylesheet" type="text/css" >
-	
-    <title>User login</title>
-	
-    <!-- Connect to Firebase so we can get stats on all Vehicles -->
-    <script src="https://www.gstatic.com/firebasejs/7.14.0/firebase-app.js"></script>
-    <script src="https://www.gstatic.com/firebasejs/7.14.0/firebase-firestore.js"></script>
-    <!-- Compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <!-- Compiled and minified JavaScript -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+	<title>User login</title>
+
+	<!-- Connect to Firebase so we can get stats on all Vehicles -->
+	<script src="https://www.gstatic.com/firebasejs/7.14.0/firebase-app.js"></script>
+	<script src="https://www.gstatic.com/firebasejs/7.14.0/firebase-firestore.js"></script>
+	<!-- Compiled and minified CSS -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	<!-- Compiled and minified JavaScript -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 	<!-- Jquery -->
 	<script type="text/javascript" src="https://code.jquery.com/jquery-1.7.1.min.js"></script>
 
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet">
-	
-<style>
-	#title
-	{
-		font-size: 30px;
-	}
-	.mySlides
-	{
-		display: none;
-	}
-	#message
-	{
-		color: red;
-		font-size: 2em;
-		font-weight: 2em;
-	}
-	input
-	{
-		color: black;
-		background: #292929;
-		text-align: center;
-	}
-	ul
-	{
-		text-align: center;
-	}
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet">
 
-	body
-	{
-		background: #8e9eab;
-		/* fallback for old browsers */
-		background: -webkit-linear-gradient(to right,
-		#eef2f3,
-		#8e9eab);
-		/* Chrome 10-25, Safari 5.1-6 */
-		background: linear-gradient(to right,
-		#eef2f3,
-		#8e9eab);
-		/* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+	<style>
+		#title
+		{
+			font-size: 30px;
+		}
+		.mySlides
+		{
+			display: none;
+		}
+		#message
+		{
+			color: red;
+			font-size: 2em;
+			font-weight: 2em;
+		}
+		input
+		{
+			color: black;
+			background: #292929;
+			text-align: center;
+		}
+		ul
+		{
+			text-align: center;
+		}
 
-		color: black;
-	}
+		body
+		{
+			background: #8e9eab;
+			/* fallback for old browsers */
+			background: -webkit-linear-gradient(to right,
+			#eef2f3,
+			#8e9eab);
+			/* Chrome 10-25, Safari 5.1-6 */
+			background: linear-gradient(to right,
+			#eef2f3,
+			#8e9eab);
+			/* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 
-	h2
-	{
+			color: black;
+		}
 
-		margin: 45px;
-		font-family: "Roboto", sans-serif;
-		font-weight: 2.5em;
-		padding: 5px;
-		font-size: 2.5em;
-
-	}
-</style>
+		h2
+		{
+			margin: 45px;
+			font-family: "Roboto", sans-serif;
+			font-weight: 2.5em;
+			padding: 5px;
+			font-size: 2.5em;
+		}
+	</style>
 </head>
 <body>
 
 <script>
-//sessionStorage.loginid = "AYAYA";
-console.log("LOGIN SESSION: "+sessionStorage.getItem("loginid"));
+	//sessionStorage.loginid = "AYAYA";
+	console.log("LOGIN SESSION: "+sessionStorage.getItem("loginid"));
 
 
-function checkLogin(login)
-{
-}
+	function checkLogin(login)
+	{
+	}
 
-</script>
-    
-    <h1>User login</h1>
+	</script>
+
+	<h1>User login</h1>
 	<p>Please login to make bookings</p>
-	
+
 	<form action="login_check_user" method="post">
 	@csrf <!-- {{ csrf_field() }} -->
 	Username: <input type="text" name="username" id="username" maxlength="20" required>
@@ -104,7 +102,7 @@ function checkLogin(login)
 	<br/><br/>
 	<button type="submit">Login</button>
 	</form>
-	
+
 	No account? <a href="register">Register</a>
 	<br/>
 	<a href="/">Back to home</a>
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -7,17 +7,83 @@ ob_start();
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<!--link href="/style.css" rel="stylesheet" type="text/css" -->
-    <title>Login</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="/style.css" rel="stylesheet" type="text/css" >
+
+	<title>User register</title>
+
+	<!-- Connect to Firebase so we can get stats on all Vehicles -->
+	<script src="https://www.gstatic.com/firebasejs/7.14.0/firebase-app.js"></script>
+	<script src="https://www.gstatic.com/firebasejs/7.14.0/firebase-firestore.js"></script>
+	<!-- Compiled and minified CSS -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	<!-- Compiled and minified JavaScript -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+	<!-- Jquery -->
+	<script type="text/javascript" src="https://code.jquery.com/jquery-1.7.1.min.js"></script>
+
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet">
+
+	<style>
+		#title
+		{
+			font-size: 30px;
+		}
+		.mySlides
+		{
+			display: none;
+		}
+		#message
+		{
+			color: red;
+			font-size: 2em;
+			font-weight: 2em;
+		}
+		input
+		{
+			color: black;
+			background: #292929;
+			text-align: center;
+		}
+		ul
+		{
+			text-align: center;
+		}
+
+		body
+		{
+			background: #8e9eab;
+			/* fallback for old browsers */
+			background: -webkit-linear-gradient(to right,
+			#eef2f3,
+			#8e9eab);
+			/* Chrome 10-25, Safari 5.1-6 */
+			background: linear-gradient(to right,
+			#eef2f3,
+			#8e9eab);
+			/* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+			color: black;
+		}
+
+		h2
+		{
+			margin: 45px;
+			font-family: "Roboto", sans-serif;
+			font-weight: 2.5em;
+			padding: 5px;
+			font-size: 2.5em;
+		}
+	</style>
 </head>
 <body>
     
-    <h1>User register</h1>
+	<h1>User register</h1>
 
 	<p>Please enter your details to create an account</p>
-	
+
 	<form action="register_check" method="post">
 	@csrf <!-- {{ csrf_field() }} -->
 	Username: <input type="text" name="username" id="username" maxlength="20" required>
@@ -27,7 +93,7 @@ ob_start();
 	<br/><br/>
 	<button type="submit">Register</button>
 	</form>
-	
+
 	Already have an account? <a href="login">Login</a>
 	<br/>
 	<a href="/">Back to home</a>
