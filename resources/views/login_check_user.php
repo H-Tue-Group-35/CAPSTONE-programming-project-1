a4dfebc315c949fb33d8e3601a6c6586bcfc1d39
GeloParlade <30657437+GeloParlade@users.noreply.github.com>
2020-06-17 08:08:13 +1000

    Added login page styling

--- resources/views/login_check_user.php
+++ resources/views/login_check_user.php
@@ -101,12 +101,12 @@ ob_start();
 			}
 			else
 			{
-				printf("Password does not match");
+				echo "<script>alert('Invalid Username/Password');window.location.href='login';</script>";
 			}
 		}
 		else
 		{	
-			printf("Error: Invalid username or password. <a href='login'>Try again</a>. <a href='/'>Back to index</a>.");
+			echo "<script>alert('Invalid Username/Password');window.location.href='login';</script>";
 		}
 	?>
 
