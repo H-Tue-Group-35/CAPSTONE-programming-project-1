c336c0397d392f051f188fb758528f6c78bf71b3
GeloParlade <30657437+GeloParlade@users.noreply.github.com>
2020-06-17 09:19:14 +1000

    update

--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -106,7 +106,7 @@ ob_start();
 
             if (userid && userid != '') {
                 document.getElementById("fHeader").innerHTML =
-                '<li style="letter-spacing: 1px;float: right;padding - right: 20 px;padding - top: 10 px;">Logged in as ' + userid + '</li><hr><li><a href="account ">ACCOUNT</a></li><li><a href="logout ">LOGOUT</a></li>';
+                '<li style="letter-spacing: 1px;float: right;padding-right: 20px;padding-top: 10px;">Logged in as ' + userid + '</li><hr><li><a href="account ">ACCOUNT</a></li><li><a href="logout ">LOGOUT</a></li>';
             } else {
                 sessionStorage.loginid = '';
                 document.getElementById("fHeader").innerHTML =
